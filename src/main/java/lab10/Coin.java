26c91ba2678349eec0ee67599197754b73a2a746
Izabela <izabela.tarasin@gmail.com>
2021-04-20 15:53:27 +0200

    update

--- src/main/java/lab10/Coin.java
+++ src/main/java/lab10/Coin.java
@@ -1,5 +1,15 @@
 package lab10;
 
 public enum Coin {
-    ONE, FIVE, TEN, TWENTY_FIVE;
+    ONE(1), FIVE(5), TEN(10), TWENTY_FIVE(25);
+
+    private int value;
+
+    Coin(int value) {
+        this.value = value;
+    }
+
+    public int getValue(){
+        return value;
+    }
 }
